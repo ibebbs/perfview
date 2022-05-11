7b16daafbddfd39f40acde63b37e262e2d328965
ibebbs <github@bebbs.co.uk>
2022-05-11 12:34:24 +0100

    Added DumpStacks

---
+++ DumpStacks/Pipeline.cs
@@ -0,0 +1,21 @@
+﻿namespace DumpStacks
+{
+    /// <summary>
+    /// Describes the ProcessorArchitecture in a SYSTEM_INFO field.
+    /// This can also be reported by a dump file.
+    /// </summary>
+    public enum ProcessorArchitecture : ushort
+    {
+        PROCESSOR_ARCHITECTURE_INTEL = 0,
+        PROCESSOR_ARCHITECTURE_MIPS = 1,
+        PROCESSOR_ARCHITECTURE_ALPHA = 2,
+        PROCESSOR_ARCHITECTURE_PPC = 3,
+        PROCESSOR_ARCHITECTURE_SHX = 4,
+        PROCESSOR_ARCHITECTURE_ARM = 5,
+        PROCESSOR_ARCHITECTURE_IA64 = 6,
+        PROCESSOR_ARCHITECTURE_ALPHA64 = 7,
+        PROCESSOR_ARCHITECTURE_MSIL = 8,
+        PROCESSOR_ARCHITECTURE_AMD64 = 9,
+        PROCESSOR_ARCHITECTURE_IA32_ON_WIN64 = 10,
+    }
+}
---
+++ DumpStacks/Program.cs
@@ -0,0 +1,6 @@
+﻿using var dumpReader = new DumpStacks.DumpReader(Environment.GetCommandLineArgs().First());
+
+if (dumpReader.IsExceptionStream())
+    Console.WriteLine("Has Exception Stream");
+else
+    Console.WriteLine("Does not have Exception Stream");
